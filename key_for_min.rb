87d755378cf2030be8bf8bb399dfd0a7af910311
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-15 17:36:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,11 @@
 
 def key_for_min_value(name_hash)
 min_value = 100
-min_key = key_for_min_value
+
   if name_hash == {}
 	  return nil
 	 else 
+	   min_key = key
 	   name_hash.each do |key, value|
 	     if value <= min_value
         min_value = value
